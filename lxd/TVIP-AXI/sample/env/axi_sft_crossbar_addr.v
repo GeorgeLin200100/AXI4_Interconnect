31caf461d8d816c7f6985a24e5dcfd2a4a5ff15e
GeorgeLin200100 <xuandalin@163.com>
2025-06-16 21:38:27 +0800

    add thread ecc protection

--- lxd/TVIP-AXI/sample/env/axi_sft_crossbar_addr.v
+++ lxd/TVIP-AXI/sample/env/axi_sft_crossbar_addr.v
@@ -270,12 +270,21 @@ reg [CL_M_COUNT-1:0] thread_m_reg[S_INT_THREADS-1:0];
 reg [3:0] thread_region_reg[S_INT_THREADS-1:0];
 reg [$clog2(S_ACCEPT+1)-1:0] thread_count_reg[S_INT_THREADS-1:0];
 
+reg [6:0] thread_ecc_prt_reg[S_INT_THREADS-1:0];  //protect thread_id_reg & thread_m_reg & thread_region_reg
+localparam THREAD_ECC_APPEND_ZERO_NUM = 64-ID_WIDTH-CL_M_COUNT-4;
+wire [THREAD_ECC_APPEND_ZERO_NUM-1:0] dummy[S_INT_THREADS-1:0];
+wire [6:0] thread_ecc_prt_dec[S_INT_THREADS-1:0];
+wire [ID_WIDTH-1:0] thread_id_dec[S_INT_THREADS-1:0];
+wire [CL_M_COUNT-1:0] thread_m_dec[S_INT_THREADS-1:0];
+wire [3:0] thread_region_dec[S_INT_THREADS-1:0];
+
 wire [S_INT_THREADS-1:0] thread_active;
 wire [S_INT_THREADS-1:0] thread_match;
 wire [S_INT_THREADS-1:0] thread_match_dest;
 wire [S_INT_THREADS-1:0] thread_cpl_match;
 wire [S_INT_THREADS-1:0] thread_trans_start;
 wire [S_INT_THREADS-1:0] thread_trans_complete;
+wire [70:0] thread_ecc_encoded[S_INT_THREADS-1:0];
 
 generate
     genvar n;
@@ -285,17 +294,24 @@ generate
             thread_count_reg[n] <= 0;
         end
 
+        assign {thread_ecc_prt_dec[n], dummy[n], thread_id_dec[n], thread_m_dec[n], thread_region_dec[n]} = ecc_d64b_p7_dec_func({thread_ecc_prt_reg[n],{THREAD_ECC_APPEND_ZERO_NUM{1'b0}},thread_id_reg[n], thread_m_reg[n], thread_region_reg[n]});
+
         // thread has ongoing operations
         assign thread_active[n] = thread_count_reg[n] != 0;
         // thread has ongoing operations and the id matches
-        assign thread_match[n] = thread_active[n] && thread_id_reg[n] == s_axi_aid;
+        //assign thread_match[n] = thread_active[n] && thread_id_reg[n] == s_axi_aid;
+        assign thread_match[n] = thread_active[n] && thread_id_dec[n] == s_axi_aid;
         // thread has ongoing operations and the id matches and the destination matches (means in the same thread)
-        assign thread_match_dest[n] = thread_match[n] && thread_m_reg[n] == m_select_next && (M_REGIONS < 2 || thread_region_reg[n] == m_axi_aregion_next);
+        //assign thread_match_dest[n] = thread_match[n] && thread_m_reg[n] == m_select_next && (M_REGIONS < 2 || thread_region_reg[n] == m_axi_aregion_next);
+        assign thread_match_dest[n] = thread_match[n] && thread_m_dec[n] == m_select_next && (M_REGIONS < 2 || thread_region_dec[n] == m_axi_aregion_next);
         // thread has ongoing operations and the completion id matches
-        assign thread_cpl_match[n] = thread_active[n] && thread_id_reg[n] == s_cpl_id;
+        //assign thread_cpl_match[n] = thread_active[n] && thread_id_reg[n] == s_cpl_id;
+        assign thread_cpl_match[n] = thread_active[n] && thread_id_dec[n] == s_cpl_id;
         // either match & use existing thread or start a new thread
         assign thread_trans_start[n] = (thread_match[n] || (!thread_active[n] && !thread_match && !(thread_trans_start & ({S_INT_THREADS{1'b1}} >> (S_INT_THREADS-n))))) && trans_start;
         assign thread_trans_complete[n] = thread_cpl_match[n] && trans_complete;
+
+        assign thread_ecc_encoded[n] = ecc_d64b_p7_enc_func({{THREAD_ECC_APPEND_ZERO_NUM{1'b0}}, s_axi_aid, m_select_next, m_axi_aregion_next});
 
         always @(posedge clk) begin
             if (rst) begin
@@ -309,9 +325,14 @@ generate
             end
 
             if (thread_trans_start[n]) begin
-                thread_id_reg[n] <= s_axi_aid;
-                thread_m_reg[n] <= m_select_next;
-                thread_region_reg[n] <= m_axi_aregion_next;
+                // thread_id_reg[n] <= s_axi_aid;
+                // thread_m_reg[n] <= m_select_next;
+                // thread_region_reg[n] <= m_axi_aregion_next;
+                thread_ecc_prt_reg[n] <= thread_ecc_encoded[n][70:64];
+                thread_id_reg[n] <= thread_ecc_encoded[n][4+CL_M_COUNT+ID_WIDTH-1:4+CL_M_COUNT];
+                thread_m_reg[n] <= thread_ecc_encoded[n][4+CL_M_COUNT-1:4];
+                thread_region_reg[n] <= thread_ecc_encoded[n][3:0];
+
             end
         end
     end
@@ -418,6 +439,58 @@ always @(posedge clk) begin
     m_select_reg <= m_select_next;
     m_decerr_reg <= m_decerr_next;
 end
+
+function [70:0] ecc_d64b_p7_enc_func;
+    input [63:0] data_in;
+
+    reg [64:1] d;
+    reg [6:0] parity_out;
+
+    begin
+        d[64:1] = data_in[63:0];
+
+        //p0-p6
+        parity_out[0] = d[1]^d[2]^d[4]^d[5]^d[7]^d[9]^d[11]^d[12]^d[14]^d[16]^d[18]^d[20]^d[22]^d[24]^d[26]^d[27]^d[29]^d[31]^d[33]^d[35]^d[37]^d[39]^d[41]^d[43]^d[45]^d[47]^d[49]^d[51]^d[53]^d[55]^d[57]^d[58]^d[60]^d[62]^d[64];
+        parity_out[1] = d[1]^d[3]^d[4]^d[6]^d[7]^d[10]^d[11]^d[13]^d[14]^d[17]^d[18]^d[21]^d[22]^d[25]^d[26]^d[28]^d[29]^d[32]^d[33]^d[36]^d[37]^d[40]^d[41]^d[44]^d[45]^d[48]^d[49]^d[52]^d[53]^d[56]^d[57]^d[59]^d[60]^d[63]^d[64];
+        parity_out[2] = d[2]^d[3]^d[4]^d[8]^d[9]^d[10]^d[11]^d[15]^d[16]^d[17]^d[18]^d[23]^d[24]^d[25]^d[26]^d[30]^d[31]^d[32]^d[33]^d[38]^d[39]^d[40]^d[41]^d[46]^d[47]^d[48]^d[49]^d[54]^d[55]^d[56]^d[57]^d[61]^d[62]^d[63]^d[64];   
+        parity_out[3] = d[5]^d[6]^d[7]^d[8]^d[9]^d[10]^d[11]^d[19]^d[20]^d[21]^d[22]^d[23]^d[24]^d[25]^d[26]^d[34]^d[35]^d[36]^d[37]^d[38]^d[39]^d[40]^d[41]^d[50]^d[51]^d[52]^d[53]^d[54]^d[55]^d[56]^d[57];
+        parity_out[4] = d[12]^d[13]^d[14]^d[15]^d[16]^d[17]^d[18]^d[19]^d[20]^d[21]^d[22]^d[23]^d[24]^d[25]^d[26]^d[42]^d[43]^d[44]^d[45]^d[46]^d[47]^d[48]^d[49]^d[50]^d[51]^d[52]^d[53]^d[54]^d[55]^d[56]^d[57];
+        parity_out[5] = d[27]^d[28]^d[29]^d[30]^d[31]^d[32]^d[33]^d[34]^d[35]^d[36]^d[37]^d[38]^d[39]^d[40]^d[41]^d[42]^d[43]^d[44]^d[45]^d[46]^d[47]^d[48]^d[49]^d[50]^d[51]^d[52]^d[53]^d[54]^d[55]^d[56]^d[57];
+        parity_out[6] = d[58]^d[59]^d[60]^d[61]^d[62]^d[63]^d[64];
+        
+        ecc_d64b_p7_enc_func = {parity_out, d[64:1]};
+    end
+endfunction
+
+function [63:0] ecc_d64b_p7_dec_func;
+    input [70:0] in;
+    reg [63:0] data_in;
+    reg [6:0] parity_in;
+    reg [64:1] d;
+    reg [6:0] parity_local;
+    reg [6:0] is_parity_diff;
+    reg [71:1] ecc_in;//!!start from 1 to 71, 0 is not used
+    reg error_flag;
+    reg [70:0] ecc_corrected;
+    begin
+        data_in[63:0] = in[63:0];
+        parity_in[6:0] = in[70:64];
+        d[64:1] = data_in[63:0];
+        parity_local[0] = d[1]^d[2]^d[4]^d[5]^d[7]^d[9]^d[11]^d[12]^d[14]^d[16]^d[18]^d[20]^d[22]^d[24]^d[26]^d[27]^d[29]^d[31]^d[33]^d[35]^d[37]^d[39]^d[41]^d[43]^d[45]^d[47]^d[49]^d[51]^d[53]^d[55]^d[57]^d[58]^d[60]^d[62]^d[64];
+        parity_local[1] = d[1]^d[3]^d[4]^d[6]^d[7]^d[10]^d[11]^d[13]^d[14]^d[17]^d[18]^d[21]^d[22]^d[25]^d[26]^d[28]^d[29]^d[32]^d[33]^d[36]^d[37]^d[40]^d[41]^d[44]^d[45]^d[48]^d[49]^d[52]^d[53]^d[56]^d[57]^d[59]^d[60]^d[63]^d[64];
+        parity_local[2] = d[2]^d[3]^d[4]^d[8]^d[9]^d[10]^d[11]^d[15]^d[16]^d[17]^d[18]^d[23]^d[24]^d[25]^d[26]^d[30]^d[31]^d[32]^d[33]^d[38]^d[39]^d[40]^d[41]^d[46]^d[47]^d[48]^d[49]^d[54]^d[55]^d[56]^d[57]^d[61]^d[62]^d[63]^d[64];   
+        parity_local[3] = d[5]^d[6]^d[7]^d[8]^d[9]^d[10]^d[11]^d[19]^d[20]^d[21]^d[22]^d[23]^d[24]^d[25]^d[26]^d[34]^d[35]^d[36]^d[37]^d[38]^d[39]^d[40]^d[41]^d[50]^d[51]^d[52]^d[53]^d[54]^d[55]^d[56]^d[57];
+        parity_local[4] = d[12]^d[13]^d[14]^d[15]^d[16]^d[17]^d[18]^d[19]^d[20]^d[21]^d[22]^d[23]^d[24]^d[25]^d[26]^d[42]^d[43]^d[44]^d[45]^d[46]^d[47]^d[48]^d[49]^d[50]^d[51]^d[52]^d[53]^d[54]^d[55]^d[56]^d[57];
+        parity_local[5] = d[27]^d[28]^d[29]^d[30]^d[31]^d[32]^d[33]^d[34]^d[35]^d[36]^d[37]^d[38]^d[39]^d[40]^d[41]^d[42]^d[43]^d[44]^d[45]^d[46]^d[47]^d[48]^d[49]^d[50]^d[51]^d[52]^d[53]^d[54]^d[55]^d[56]^d[57];
+        parity_local[6] = d[58]^d[59]^d[60]^d[61]^d[62]^d[63]^d[64];
+        is_parity_diff = parity_local ^ parity_in;
+        {ecc_in[64],ecc_in[32],ecc_in[16],ecc_in[8],ecc_in[4],ecc_in[2],ecc_in[1]} = parity_in[6:0];
+        {ecc_in[71:65],ecc_in[63:33],ecc_in[31:17],ecc_in[15:9],ecc_in[7:5],ecc_in[3]} = d[64:1];
+        error_flag = |is_parity_diff;
+        ecc_corrected = (error_flag) ? (ecc_in ^ (1 << (is_parity_diff[6:0]-1))) : ecc_in;
+        ecc_d64b_p7_dec_func = {ecc_corrected[70:64],ecc_corrected[62-:31],ecc_corrected[30-:15],ecc_corrected[14-:7],ecc_corrected[6-:3],ecc_corrected[2]};
+    end
+endfunction
 
 endmodule
 
